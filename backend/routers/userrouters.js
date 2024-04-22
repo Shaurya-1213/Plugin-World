91e9b5586117427b5b32c559f898c6244594b561
Shaurya-1213 <shauryasingh2033@gmail.com>
2024-04-22 16:39:34 -0400

    backend

--- backend/routers/userrouters.js
+++ backend/routers/userrouters.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 router.get('/add',(req,res)=>{
   res.send('Response from user add');
-})
+});
+
+//getall
+//getbyid
+//delete
+//update
 
 module.exports = router;
