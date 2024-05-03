24ef3968488a26f0af7fa014d53107797e16ca0e
Shaurya-1213 <shauryasingh2033@gmail.com>
2024-05-03 02:38:04 -0400

    sdf

--- frontend/src/app/(main)/contact/page.jsx
+++ frontend/src/app/(main)/contact/page.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div>
       <>
   {/* component */}
-  <div className="py-4 bg-voilet-600 text-white">
+  <div className="py-4 bg-voilet-900 text-white">
     <div className="container mx-auto flex flex-col md:flex-row my-12 md:my-36">
       <div className="flex flex-col w-full lg:w-2/5 p-8">
         <p className="text-3xl md:text-5xl text-yellow-500 my-4 leading-relaxed md:leading-snug">
@@ -52,7 +52,7 @@ const Contact = () => {
       <div className=" flex flex-col lg:w-3/5 justify-center w-full lg:-mt-12">
         <div className="container">
           <div className="relative flex flex-col min-w-0 break-words w-full">
-            <div className="flex-auto p-5 lg:p-10">
+            <div className="flex-auto lg:p-10">
               <img
                 src="https://user-images.githubusercontent.com/54521023/152731049-cc744a56-1d6f-4945-9566-0fa3b7ad1d24.png"
                 alt="contact image"
@@ -63,6 +63,284 @@ const Contact = () => {
       </div>
     </div>
   </div>
+</>
+<>
+  {/* Contact */}
+  <div className="bg-voilet-900">
+    <div className="max-w-5xl px-4 xl:px-0 pb-24 mx-auto"> 
+      {/* Title */}
+      <div className="max-w-3xl mb-10 lg:mb-14">
+        <h2 className="text-white font-semibold text-2xl md:text-4xl md:leading-tight">
+          Contact us
+        </h2>
+        <p className="mt-1 text-neutral-400">
+          Whatever your goal - we will get you there.
+        </p>
+      </div>
+      {/* End Title */}
+      {/* Grid */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 lg:gap-x-16">
+        <div className="md:order-2 border-b border-neutral-800 pb-10 mb-10 md:border-b-0 md:pb-0 md:mb-0">
+          <form>
+            <div className="space-y-4">
+              {/* Input */}
+              <div className="relative">
+                <input
+                  type="text"
+                  id="hs-tac-input-name"
+                  className="peer p-4 block w-full bg-neutral-800 border-transparent rounded-lg text-sm text-white placeholder:text-transparent focus:outline-none focus:ring-0 focus:border-transparent disabled:opacity-50 disabled:pointer-events-none
+        focus:pt-6
+        focus:pb-2
+        [&:not(:placeholder-shown)]:pt-6
+        [&:not(:placeholder-shown)]:pb-2
+        autofill:pt-6
+        autofill:pb-2"
+                  placeholder="Name"
+                />
+                <label
+                  htmlFor="hs-tac-input-name"
+                  className="absolute top-0 start-0 p-4 h-full text-neutral-400 text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent peer-disabled:opacity-50 peer-disabled:pointer-events-none
+          peer-focus:text-xs
+          peer-focus:-translate-y-1.5
+          peer-focus:text-neutral-400
+          peer-[:not(:placeholder-shown)]:text-xs
+          peer-[:not(:placeholder-shown)]:-translate-y-1.5
+          peer-[:not(:placeholder-shown)]:text-neutral-400"
+                >
+                  Name
+                </label>
+              </div>
+              {/* End Input */}
+              {/* Input */}
+              <div className="relative">
+                <input
+                  type="email"
+                  id="hs-tac-input-email"
+                  className="peer p-4 block w-full bg-neutral-800 border-transparent rounded-lg text-sm text-white placeholder:text-transparent focus:outline-none focus:ring-0 focus:border-transparent disabled:opacity-50 disabled:pointer-events-none
+        focus:pt-6
+        focus:pb-2
+        [&:not(:placeholder-shown)]:pt-6
+        [&:not(:placeholder-shown)]:pb-2
+        autofill:pt-6
+        autofill:pb-2"
+                  placeholder="Email"
+                />
+                <label
+                  htmlFor="hs-tac-input-email"
+                  className="absolute top-0 start-0 p-4 h-full text-neutral-400 text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent peer-disabled:opacity-50 peer-disabled:pointer-events-none
+          peer-focus:text-xs
+          peer-focus:-translate-y-1.5
+          peer-focus:text-neutral-400
+          peer-[:not(:placeholder-shown)]:text-xs
+          peer-[:not(:placeholder-shown)]:-translate-y-1.5
+          peer-[:not(:placeholder-shown)]:text-neutral-400"
+                >
+                  Email
+                </label>
+              </div>
+              {/* End Input */}
+              {/* Input */}
+              <div className="relative">
+                <input
+                  type="text"
+                  id="hs-tac-input-company"
+                  className="peer p-4 block w-full bg-neutral-800 border-transparent rounded-lg text-sm text-white placeholder:text-transparent focus:outline-none focus:ring-0 focus:border-transparent disabled:opacity-50 disabled:pointer-events-none
+        focus:pt-6
+        focus:pb-2
+        [&:not(:placeholder-shown)]:pt-6
+        [&:not(:placeholder-shown)]:pb-2
+        autofill:pt-6
+        autofill:pb-2"
+                  placeholder="Company"
+                />
+                <label
+                  htmlFor="hs-tac-input-company"
+                  className="absolute top-0 start-0 p-4 h-full text-neutral-400 text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent peer-disabled:opacity-50 peer-disabled:pointer-events-none
+          peer-focus:text-xs
+          peer-focus:-translate-y-1.5
+          peer-focus:text-neutral-400
+          peer-[:not(:placeholder-shown)]:text-xs
+          peer-[:not(:placeholder-shown)]:-translate-y-1.5
+          peer-[:not(:placeholder-shown)]:text-neutral-400"
+                >
+                  Company
+                </label>
+              </div>
+              {/* End Input */}
+              {/* Input */}
+              <div className="relative">
+                <input
+                  type="text"
+                  id="hs-tac-input-phone"
+                  className="peer p-4 block w-full bg-neutral-800 border-transparent rounded-lg text-sm text-white placeholder:text-transparent focus:outline-none focus:ring-0 focus:border-transparent disabled:opacity-50 disabled:pointer-events-none
+        focus:pt-6
+        focus:pb-2
+        [&:not(:placeholder-shown)]:pt-6
+        [&:not(:placeholder-shown)]:pb-2
+        autofill:pt-6
+        autofill:pb-2"
+                  placeholder="Phone"
+                />
+                <label
+                  htmlFor="hs-tac-input-phone"
+                  className="absolute top-0 start-0 p-4 h-full text-neutral-400 text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent peer-disabled:opacity-50 peer-disabled:pointer-events-none
+          peer-focus:text-xs
+          peer-focus:-translate-y-1.5
+          peer-focus:text-neutral-400
+          peer-[:not(:placeholder-shown)]:text-xs
+          peer-[:not(:placeholder-shown)]:-translate-y-1.5
+          peer-[:not(:placeholder-shown)]:text-neutral-400"
+                >
+                  Phone
+                </label>
+              </div>
+              {/* End Input */}
+              {/* Textarea */}
+              <div className="relative">
+                <textarea
+                  id="hs-tac-message"
+                  className="peer p-4 block w-full bg-neutral-800 border-transparent rounded-lg text-sm text-white placeholder:text-transparent focus:outline-none focus:ring-0 focus:border-transparent disabled:opacity-50 disabled:pointer-events-none
+        focus:pt-6
+        focus:pb-2
+        [&:not(:placeholder-shown)]:pt-6
+        [&:not(:placeholder-shown)]:pb-2
+        autofill:pt-6
+        autofill:pb-2"
+                  placeholder="This is a textarea placeholder"
+                  defaultValue={""}
+                />
+                <label
+                  htmlFor="hs-tac-message"
+                  className="absolute top-0 start-0 p-4 h-full text-neutral-400 text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent peer-disabled:opacity-50 peer-disabled:pointer-events-none
+          peer-focus:text-xs
+          peer-focus:-translate-y-1.5
+          peer-focus:text-neutral-400
+          peer-[:not(:placeholder-shown)]:text-xs
+          peer-[:not(:placeholder-shown)]:-translate-y-1.5
+          peer-[:not(:placeholder-shown)]:text-neutral-400"
+                >
+                  Tell us about your project
+                </label>
+              </div>
+              {/* End Textarea */}
+            </div>
+            <div className="mt-2">
+              <p className="text-xs text-neutral-500">
+                All fields are required
+              </p>
+              <p className="mt-5">
+                <a
+                  className="group inline-flex items-center gap-x-2 py-2 px-3 bg-[#ff0] font-medium text-sm text-neutral-800 rounded-full focus:outline-none"
+                  href="#"
+                >
+                  Submit
+                  <svg
+                    className="flex-shrink-0 size-4 transition group-hover:translate-x-0.5 group-hover:translate-x-0 group-focus:translate-x-0.5 group-focus:translate-x-0"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={24}
+                    height={24}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M5 12h14" />
+                    <path d="m12 5 7 7-7 7" />
+                  </svg>
+                </a>
+              </p>
+            </div>
+          </form>
+        </div>
+        {/* End Col */}
+        <div className="space-y-14">
+          {/* Item */}
+          <div className="flex gap-x-5">
+            <svg
+              className="flex-shrink-0 size-6 text-neutral-500"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M21.2 8.4c.5.38.8.97.8 1.6v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V10a2 2 0 0 1 .8-1.6l8-6a2 2 0 0 1 2.4 0l8 6Z" />
+              <path d="m22 10-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 10" />
+            </svg>
+            <div className="grow">
+              <h4 className="text-white font-semibold">Email us:</h4>
+              <a
+                className="mt-1 text-neutral-400 text-sm"
+                href="#mailto:example@site.co"
+                target="_blank"
+              >
+                hello@example.so
+              </a>
+            </div>
+          </div>
+          {/* End Item */}
+          {/* Item */}
+          <div className="flex gap-x-5">
+            <svg
+              className="flex-shrink-0 size-6 text-neutral-500"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m3 11 18-5v12L3 14v-3z" />
+              <path d="M11.6 16.8a3 3 0 1 1-5.8-1.6" />
+            </svg>
+            <div className="grow">
+              <h4 className="text-white font-semibold">We're hiring</h4>
+              <p className="mt-1 text-neutral-400">
+                We're thrilled to announce that we're expanding our team and
+                looking for talented individuals like you to join us.
+              </p>
+              <p className="mt-2">
+                <a
+                  className="group inline-flex items-center gap-x-2 font-medium text-sm text-[#ff0] decoration-2 hover:underline focus:outline-none focus:underline"
+                  href="#"
+                >
+                  Job openings
+                  <svg
+                    className="flex-shrink-0 size-4 transition group-hover:translate-x-0.5 group-hover:translate-x-0 group-focus:translate-x-0.5 group-focus:translate-x-0"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={24}
+                    height={24}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M5 12h14" />
+                    <path d="m12 5 7 7-7 7" />
+                  </svg>
+                </a>
+              </p>
+            </div>
+          </div>
+          {/* End Item */}
+        </div>
+        {/* End Col */}
+      </div>
+      {/* End Grid */}
+    </div>
+  </div>
+  {/* End Contact */}
 </>
 
 
--- plugins/src/AddProduct.jsx
+++ plugins/src/AddProduct.jsx
@@ -13,8 +13,6 @@ const addProductSchema = Yup.object().shape({
 
 const AddProduct = () => {
 
-  const router = useRouter();
-
   const addproductForm = useFormik({
     initialValues: {
       username: '',
@@ -194,6 +192,206 @@ const AddProduct = () => {
           </form>
         </div>
       </section>
+     
+
+     {/*example*/}
+     <>
+  {/* Card Section */}
+  <div className="max-w-4xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+    <form>
+      {/* Card */}
+      <div className="bg-white rounded-xl shadow dark:bg-neutral-900">
+        <div className="relative h-40 rounded-t-xl bg-[url('https://preline.co/assets/svg/examples/abstract-bg-1.svg')] bg-no-repeat bg-cover bg-center">
+          <div className="absolute top-0 end-0 p-4">
+            <button
+              type="button"
+              className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
+            >
+              <svg
+                className="flex-shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width={24}
+                height={24}
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                <polyline points="17 8 12 3 7 8" />
+                <line x1={12} x2={12} y1={3} y2={15} />
+              </svg>
+              Upload header
+            </button>
+          </div>
+        </div>
+        <div className="pt-0 p-4 sm:pt-0 sm:p-7">
+          {/* Grid */}
+          <div className="space-y-4 sm:space-y-6">
+            <div>
+              <label className="sr-only">Product photo</label>
+              <div className="grid sm:flex sm:items-center sm:gap-x-5">
+                <img
+                  className="-mt-8 relative z-10 inline-block size-24 mx-auto sm:mx-0 rounded-full ring-4 ring-white dark:ring-neutral-900"
+                  src="../assets/img/160x160/img1.jpg"
+                  alt="Image Description"
+                />
+                <div className="mt-4 sm:mt-auto sm:mb-1.5 flex justify-center sm:justify-start gap-2">
+                  <button
+                    type="button"
+                    className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
+                  >
+                    <svg
+                      className="flex-shrink-0 size-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={24}
+                      height={24}
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                      <polyline points="17 8 12 3 7 8" />
+                      <line x1={12} x2={12} y1={3} y2={15} />
+                    </svg>
+                    Upload logo
+                  </button>
+                  <button
+                    type="button"
+                    className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-red-500 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:hover:bg-neutral-800"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="af-submit-app-project-name"
+                className="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-neutral-200"
+              >
+                Project name
+              </label>
+              <input
+                id="af-submit-app-project-name"
+                type="text"
+                className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                placeholder="Enter project name"
+              />
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="af-submit-project-url"
+                className="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-neutral-200"
+              >
+                Project URL
+              </label>
+              <input
+                id="af-submit-project-url"
+                type="text"
+                className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                placeholder="https://example.so"
+              />
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="af-submit-app-upload-images"
+                className="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-neutral-200"
+              >
+                Preview image
+              </label>
+              <label
+                htmlFor="af-submit-app-upload-images"
+                className="group p-4 sm:p-7 block cursor-pointer text-center border-2 border-dashed border-gray-200 rounded-lg focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 dark:border-neutral-700"
+              >
+                <input
+                  id="af-submit-app-upload-images"
+                  name="af-submit-app-upload-images"
+                  type="file"
+                  className="sr-only"
+                />
+                <svg
+                  className="size-10 mx-auto text-gray-400 dark:text-neutral-600"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={16}
+                  height={16}
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2z"
+                  />
+                  <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z" />
+                </svg>
+                <span className="mt-2 block text-sm text-gray-800 dark:text-neutral-200">
+                  Browse your device or{" "}
+                  <span className="group-hover:text-blue-700 text-blue-600">
+                    drag 'n drop'
+                  </span>
+                </span>
+                <span className="mt-1 block text-xs text-gray-500 dark:text-neutral-500">
+                  Maximum file size is 2 MB
+                </span>
+              </label>
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="af-submit-app-category"
+                className="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-neutral-200"
+              >
+                Category
+              </label>
+              <select
+                id="af-submit-app-category"
+                className="py-2 px-3 pe-9 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+              >
+                <option selected="">Select a category</option>
+                <option>Ecommerce</option>
+                <option>Finance</option>
+                <option>Marketplace</option>
+                <option>Social</option>
+                <option>Others</option>
+              </select>
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="af-submit-app-description"
+                className="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-neutral-200"
+              >
+                Description
+              </label>
+              <textarea
+                id="af-submit-app-description"
+                className="py-2 px-3 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                rows={6}
+                placeholder="A detailed summary will better explain your products to the audiences. Our users will see this in your dedicated product page."
+                defaultValue={""}
+              />
+            </div>
+          </div>
+          {/* End Grid */}
+          <div className="mt-5 flex justify-center gap-x-2">
+            <button
+              type="button"
+              className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+            >
+              Submit your project
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* End Card */}
+    </form>
+  </div>
+  {/* End Card Section */}
+</>
+
 
     </>
   )
