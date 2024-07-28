31cfc5c5466477bd1e322d95066462a9ef506b79
Shaurya Singh <163731296+Shaurya-1213@users.noreply.github.com>
2024-07-28 22:23:16 +0530

    Update page.jsx

--- frontend/src/app/termsOfService/page.jsx
+++ frontend/src/app/termsOfService/page.jsx
@@ -5,7 +5,7 @@ const termsOfService = () => {
     <>
     <div className="bg-white py-6 sm:py-8 lg:py-12">
   <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-    {/* text - start  */}
+    {/* text - start hj */}
     <div className="mb-10 md:mb-16">
       <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
         Frequently asked questions
