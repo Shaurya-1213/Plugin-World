6b405e333b85a9dd9ae1bdb93d81fc7e31a7d665
Shaurya Singh <163731296+Shaurya-1213@users.noreply.github.com>
2024-08-02 13:23:45 +0530

    Update page.jsx

--- frontend/src/app/termsOfService/page.jsx
+++ frontend/src/app/termsOfService/page.jsx
@@ -13,7 +13,7 @@ const termsOfService = () => {
       <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
         This is a section of some simple filler text, also known as placeholder
         text. It shares some characteristics of a real written text but is
-        random or otherwise generated.
+        random or otherwise generaated.
       </p>
     </div>
     {/* text - end */}
