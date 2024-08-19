7b4499c570ebe8e6e2d44ccacca7da9d86f0a404
Shaurya Singh <163731296+Shaurya-1213@users.noreply.github.com>
2024-08-20 04:37:42 +0530

    Update page.jsx

--- frontend/src/app/termsOfService/page.jsx
+++ frontend/src/app/termsOfService/page.jsx
@@ -5,7 +5,7 @@ const termsOfService = () => {
     <>
     <div className="bg-white py-6 sm:py-8 lg:py-12">
   <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-    {/* text - start  */}f
+    {/* text - start  */}
     <div className="mb-10 md:mb-16">
       <iframe src="https://assets.pinterest.com/ext/embed.html?id=12525705205838911" height="550" width="450" frameborder="0" scrolling="no" ></iframe>
 
